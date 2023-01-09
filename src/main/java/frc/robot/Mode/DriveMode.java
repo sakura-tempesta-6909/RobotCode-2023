1f7daac085573828e3c69fa5ec7d8ca86efcdafc
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2023-01-09 21:37:09 +0900

    driveBaseを動かす

---
+++ src/main/java/frc/robot/Mode/DriveMode.java
@@ -0,0 +1,20 @@
+package frc.robot.Mode;
+
+import frc.robot.State;
+
+public class DriveMode extends Mode {
+
+    @Override
+    public void changeMode() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void changeState() {
+        State.driveXSpeed = driveController.getLeftY();
+        State.driveZRotation = driveController.getRightX();
+        
+    }
+    
+}
---
+++ src/main/java/frc/robot/Mode/Mode.java
@@ -0,0 +1,22 @@
+package frc.robot.Mode;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+public abstract class Mode {
+    static XboxController driveController, operateController;
+    public static void addController(XboxController driveController, XboxController operateController) {
+        Mode.driveController = driveController;
+        Mode.operateController = operateController;
+
+    }
+
+    /**
+     * Modeを変化させる。
+     */
+    abstract public void changeMode();
+
+    /**
+     * Stateを変化させる
+     */
+    abstract public void changeState();
+}
