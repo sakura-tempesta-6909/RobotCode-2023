59e7093a379a502778c5099895daa3463e42f755
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2023-01-10 18:15:31 +0900

    Constの追加

---
+++ src/main/java/frc/robot/SubClass/Const.java
@@ -0,0 +1,22 @@
+package frc.robot.SubClass;
+
+public class Const {
+    public static final class Ports {
+        public static final int DriveController = 0;
+        public static final int OperateController = 1;
+
+        public static final int DriveRightFront = 0;
+        public static final int DriveLeftFront = 1;
+        public static final int DriveRightBack = 2;
+        public static final int DriveLeftBack = 3;
+    }
+
+    public static final class Speeds {
+        public static double Neutral = 0;
+
+        public static double FastDrive = 0.8;
+        public static double MidDrive = 0.5;
+        public static double SlowDrive = 0.3;
+
+    }
+}
