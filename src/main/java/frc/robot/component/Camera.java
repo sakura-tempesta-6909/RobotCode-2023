1ba5e7dd50b9f6c4c32a769f135b39e93e9ba9ce
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2023-02-16 19:55:52 +0900

    apriltag検出

---
+++ src/main/java/frc/robot/component/Camera.java
@@ -0,0 +1,44 @@
+package frc.robot.component;
+import edu.wpi.first.apriltag.AprilTagDetection;
+
+public class Camera implements Component{
+        
+    
+
+    @Override
+    public void autonomousInit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void teleopInit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void disabledInit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void testInit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void readSensors() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void applyState() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
