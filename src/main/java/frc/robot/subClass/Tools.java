fd0aff982dac534b93331c697662147bef941ada
flet-pro <63170fu@seiko.ac.jp>
2023-02-26 16:07:29 +0900

    fix all

--- src/main/java/frc/robot/subClass/Tools.java
+++ src/main/java/frc/robot/subClass/Tools.java
@@ -16,7 +16,7 @@ public class Tools {
         double l1 = Const.Arm.RootArmLength;
         double l2 = Const.Arm.HeadArmLength;
 
-        return l1 * Math.cos(RootAngle) - l2 * Math.cos(SumAngle);
+        return l1 * Math.sin(RootAngle) + l2 * Math.sin(SumAngle);
     }
 
     /**
@@ -31,7 +31,7 @@ public class Tools {
         double l1 = Const.Arm.RootArmLength;
         double l2 = Const.Arm.HeadArmLength;
 
-        return -1 * l1 * Math.sin(RootAngle) + l2 * Math.sin(SumAngle);
+        return l1 * Math.cos(RootAngle) + l2 * Math.cos(SumAngle);
     }
 
     private static final double deadZoneThreshold = 0.05;
@@ -48,33 +48,49 @@ public class Tools {
     }
 
     /**
-     * @param Height ターゲットのX座標[cm]
-     * @param Depth ターゲットのZ座標[cm]
-     * X,ZからRootAngle, JointAngle を計算
+     * @param Height ターゲットのY座標[cm]
+     * @param Depth ターゲットのX座標[cm]
+     * X,YからRootAngle, JointAngle を計算
      * @return アームのターゲットの角度[deg]
      */
     public static Map<String, Double> calculateAngles(double Height, double Depth) {
         double l1 = Const.Arm.RootArmLength;
         double l2 = Const.Arm.HeadArmLength;
 
-        double JointAngle = Math.acos((Math.pow(l1, 2) + Math.pow(l2, 2)
-                - Math.pow(Height, 2) - Math.pow(Depth, 2)) / (2 * l1 * l2));
+        double HeightPM = Math.signum(Height);
+        double DepthPM = Math.signum(Height);
+        Height = Math.abs(Height);
+        Depth = Math.abs(Depth);
 
-        double tX = l1 - l2 * Math.cos(JointAngle);
+        double JointAngle = Math.acos(Math.min((Math.pow(l1, 2) + Math.pow(l2, 2)
+                - Math.pow(Height, 2) - Math.pow(Depth, 2)) / (2 * l1 * l2), 1.0));
+
+        double tX = l1 + l2 * Math.cos(JointAngle);
         double tY = l2 * Math.sin(JointAngle);
         double r = Math.sqrt(Math.pow(tX, 2) + Math.pow(tY, 2));
 
-        double alphaSin = Math.asin(tX / r);
-        double RootAngleSin = Math.asin(Depth / r) - alphaSin - 90;
-        double alphaCos = Math.acos(tX / r);
-        double RootAngleCos = Math.acos(Depth / r) + alphaCos - 90;
-
-        double RootAngle = Math.abs(Tools.calculateHeight(RootAngleSin, JointAngle) - Height) > Math.abs(Tools.calculateHeight(RootAngleCos, JointAngle) - Height)
-                ? RootAngleCos : RootAngleSin;
+        double alpha = Math.acos(tX / r);
 
         Map<String, Double> angles = new HashMap<String, Double>();
-        angles.put("RootAngle", Math.toDegrees(RootAngle));
-        angles.put("JointAngle", Math.toDegrees(JointAngle));
+
+        // theta2 < 0の時
+        double RootAngleM = Math.acos(Depth / r) + alpha;
+        // theta2 > 0の時
+        double RootAngleP = Math.acos(Depth / r) - alpha;
+
+        if (HeightPM >= 0 && DepthPM >= 0) {
+            angles.put("RootAngle", Math.toDegrees(RootAngleM));
+            angles.put("JointAngle", Math.toDegrees(-1 * JointAngle));
+        } else if (HeightPM < 0 && DepthPM >= 0) {
+            angles.put("RootAngle", Math.toDegrees(-1 * RootAngleP));
+            angles.put("JointAngle", Math.toDegrees(-1 * JointAngle));
+        } else if (HeightPM < 0 && DepthPM < 0) {
+            angles.put("RootAngle", Math.toDegrees(RootAngleP) - 180);
+            angles.put("JointAngle", Math.toDegrees(-1 * JointAngle));
+        } else {
+            angles.put("RootAngle", 180 - Math.toDegrees(RootAngleP));
+            angles.put("JointAngle", Math.toDegrees(JointAngle));
+        }
 
         return angles;
     }
@@ -92,13 +108,13 @@ public class Tools {
         double SumAngle = RootAngle + JointAngle;
         double l1 = Const.Arm.RootArmLength;
         double l2 = Const.Arm.HeadArmLength;
-        double fb = Const.Arm.RootArmBarycenter; //FirstBarycenter -> fb
-        double sb = Const.Arm.HeadArmBarycenter; //SecondBarycenter -> sb
+        double b1 = Const.Arm.RootArmBarycenter; //FirstBarycenter -> fb
+        double b2 = Const.Arm.HeadArmBarycenter; //SecondBarycenter -> sb
         double m1 = Const.Arm.RootArmMass;
         double m2 = Const.Arm.HeadArmMass;
 
-        double ffMomentForFirstArm = fb * m1 * Math.cos(RootAngle);
-        double ffMomentForSecondArm = l1 * m2 * Math.sin(RootAngle) - sb * m2 * Math.sin(SumAngle);
+        double ffMomentForFirstArm = b1 * m1 * Math.cos(RootAngle);
+        double ffMomentForSecondArm = l1 * m2 * Math.cos(RootAngle) + b2 * m2 * Math.cos(SumAngle);
 
         //TODO feedforwardでmotor.setに渡す値はトルクの計算が必要
         return ffMomentForFirstArm + ffMomentForSecondArm;
@@ -115,10 +131,10 @@ public class Tools {
         RootAngle = Math.toRadians(RootAngle);
         JointAngle = Math.toRadians(JointAngle);
         double SumAngle = RootAngle + JointAngle;
-        double sb = Const.Arm.HeadArmBarycenter; //SecondBarycenter -> sb
+        double b2 = Const.Arm.HeadArmBarycenter; //SecondBarycenter -> sb
         double m2 = Const.Arm.HeadArmMass;
 
-        return sb * m2 * -1 * Math.cos(SumAngle);
+        return b2 * m2 * Math.cos(SumAngle);
     }
 
     /**
