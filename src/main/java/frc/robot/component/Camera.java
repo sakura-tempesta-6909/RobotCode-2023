99062290626d606de7f9716482c228b62ceeba5f
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2023-03-04 17:39:04 +0900

    camera関係いらないやつ消した

--- src/main/java/frc/robot/component/Camera.java
+++ src/main/java/frc/robot/component/Camera.java
@@ -1,54 +1,11 @@
 package frc.robot.component;
 
-import edu.wpi.first.apriltag.AprilTagDetection;
-import edu.wpi.first.apriltag.AprilTagDetector;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.CvSink;
-import edu.wpi.first.cscore.CvSource;
-import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.State;
 import frc.robot.subClass.Const;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import edu.wpi.first.math.controller.PIDController;
 
 public class Camera implements Component {
-
-    AprilTagDetector detector;
-    UsbCamera camera;
-    CvSink cvSink;
-    CvSource outputStream;
-    Mat mat;
-    Mat grayMat;
-    ArrayList<Integer> tags;
-    Scalar outlineColor;
-    Scalar xColor;
-    AprilTagDetection[] detections;
-
-
     public Camera() {
-        detector = new AprilTagDetector();
-        detector.addFamily("tag16h5", 0);
-
-        camera = CameraServer.startAutomaticCapture();
-        camera.setResolution(640, 480);
-
-        cvSink = CameraServer.getVideo();
-
-        outputStream = CameraServer.putVideo("detect", 640, 480);
-
-        mat = new Mat();
-        grayMat = new Mat();
-        tags = new ArrayList<>();
-
-        outlineColor = new Scalar(0, 255, 0);
-        xColor = new Scalar(0, 0, 255);
 
     }
 
