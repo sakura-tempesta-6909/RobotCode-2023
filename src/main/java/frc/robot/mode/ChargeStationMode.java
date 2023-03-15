91d884eaeb86a2e5d7ec684bebddb07f8212394c
flet-pro <63170fu@seiko.ac.jp>
2023-03-15 19:44:22 +0900

    ChargeStationModeの実装

---
+++ src/main/java/frc/robot/mode/ChargeStationMode.java
@@ -0,0 +1,14 @@
+package frc.robot.mode;
+
+public class ChargeStationMode extends Mode{
+// 正式名称は「ワクワクドキドキ神様お願いブレイクモード」
+    @Override
+    public void changeMode() {
+
+    }
+
+    @Override
+    public void changeState() {
+
+    }
+}
