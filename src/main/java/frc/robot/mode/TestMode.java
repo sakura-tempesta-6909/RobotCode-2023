36c396d5eca177bdacb4c8cf95e557806a1ac2ab
Teraterayuki <tenko0316yuki1@gmail.com>
2023-03-19 20:48:07 +0900

    TestMode実装
    (cherry picked from commit 96b8e2400b900858bd885411124309c76633e1ad)

--- src/main/java/frc/robot/mode/TestMode.java
+++ src/main/java/frc/robot/mode/TestMode.java
@@ -1,10 +1,20 @@
 package frc.robot.mode;
 
+import edu.wpi.first.math.util.Units;
+import frc.robot.State;
+import org.opencv.calib3d.StereoBM;
+
 public class TestMode extends Mode{
     @Override
     public void changeMode() {}
     
     @Override
     public void changeState() {
+        if(driveController.getAButtonPressed()){
+            State.Drive.resetPIDController = true;
+        }
+        if(driveController.getAButton()){
+            State.Drive.targetMeter = 1;
+        }
     }
 }
