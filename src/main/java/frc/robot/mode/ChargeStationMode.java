6c6b6a2527468143987ce0dc2fc4c7f23c3057dc
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2023-03-23 17:20:06 -1000

    魔法の言葉ついか

--- src/main/java/frc/robot/mode/ChargeStationMode.java
+++ src/main/java/frc/robot/mode/ChargeStationMode.java
@@ -1,5 +1,7 @@
 package frc.robot.mode;
 
+import java.util.Map;
+
 import frc.robot.States.State;
 import frc.robot.subClass.*;
 
@@ -28,6 +30,34 @@ public class ChargeStationMode extends Mode{
             State.Arm.state = State.Arm.States.s_moveArmToSpecifiedPosition;
             State.Arm.targetHeight = Const.GrabGamePiecePhase.armCubeIntakeHeight;
             State.Arm.targetDepth = Const.GrabGamePiecePhase.armCubeIntakeDepth;
+        } else if (joystick.getRawButton(2)) {
+            // すべてBasicPositionに戻る
+            State.Arm.state = State.Arm.States.s_moveArmToSpecifiedPosition;
+            State.moveLeftAndRightArmState = State.MoveLeftAndRightArmState.s_movetomiddle;
+            State.Hand.rotateState = State.Hand.RotateState.s_turnHandBack;
+            State.Arm.targetHeight = Const.Arm.InitialHeight;
+            State.Arm.targetDepth = Const.Arm.InitialDepth;
         }
+
+        if (joystick.getRawButtonPressed(1)) {
+            State.Arm.resetPidController = true;
+        } else if (joystick.getRawButtonPressed(2)) {
+            State.Arm.resetPidController = true;
+        }
+
+          // ターゲット座標からターゲットの角度を計算する
+          Map<String, Double> targetAngles = Tools.calculateAngles(State.Arm.targetDepth, State.Arm.targetHeight);
+          Double target = targetAngles.get("RootAngle");
+          if(target != null) {
+              State.Arm.targetRootAngle = target;
+          } else {
+              State.Arm.targetRootAngle = State.Arm.actualRootAngle;
+          }
+          target = targetAngles.get("JointAngle");
+          if(target != null) {
+              State.Arm.targetJointAngle = target;
+          } else {
+              State.Arm.targetJointAngle = State.Arm.actualJointAngle;
+          }
     }
 }
