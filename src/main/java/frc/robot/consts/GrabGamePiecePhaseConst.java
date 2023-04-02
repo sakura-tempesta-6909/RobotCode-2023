bbb7085dd3bb40dc403d5a466f01f14a164faf63
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2023-04-02 20:57:07 +0900

    数字変更

--- src/main/java/frc/robot/consts/GrabGamePiecePhaseConst.java
+++ src/main/java/frc/robot/consts/GrabGamePiecePhaseConst.java
@@ -4,31 +4,30 @@ public class GrabGamePiecePhaseConst {
     /**
      * 　キューブを掴むアームの高さ
      */
-    public static final double armCubeIntakeHeight = -100;
+    public static final double armCubeIntakeHeight = -110;
     /**
      * キューブを掴むアームの奥行き
      */
-    public static final double armCubeIntakeDepth = -8;
+    public static final double armCubeIntakeDepth = -3;
 
     /**
      * 　コーンを掴むアームの高さ
      */
-    public static final double armConeIntakeRelesaseHeight = -85;
+    public static final double armConeIntakeRelesaseHeight = -103;
     /**
      * 　コーンを掴むアームの高さ
      */
-    public static final double armConeIntakeHeight = -103;
+    public static final double armConeIntakeHeight = -115;
     /**
      * コーンを掴むアームの奥行き
      */
-    public static final double armConeIntakeDepth = -8;
+    public static final double armConeIntakeDepth = -7;
 
     public static final double armRelayPointHeight = 0;
     public static final double armRelayPointDepth = 60;
 
     public static final double armSubStationHeight = -7;
     public static final double armSubStationDepth = 56;
-
     public static void GrabGamePiecePhaseConstInit() {
 
     }
