c9ca0bd245b7636b2f3f1a1af39e80a8eb363d95
flet-pro <100826481+flet-pro@users.noreply.github.com>
2023-04-02 18:06:50 +0900

    角度計算修正 ver.3 (#76)

--- src/main/java/frc/robot/subClass/Tools.java
+++ src/main/java/frc/robot/subClass/Tools.java
@@ -1,37 +1,72 @@
 package frc.robot.subClass;
+
 import frc.robot.States.State;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Tools {
     /**
-     * @param RootAngle readSensorで取得した実際の角度[deg]
-     * @param JointAngle readSensorで取得した実際の角度[deg]
-     * @return Height座標[cm]
-     * */
-    public static double calculateHeight(double RootAngle, double JointAngle) {
-        RootAngle = Math.toRadians(RootAngle);
-        JointAngle = Math.toRadians(JointAngle);
-        double SumAngle = RootAngle + JointAngle;
-        double l1 = Const.Arm.RootArmLength;
-        double l2 = Const.Arm.HeadArmLength;
-
-        return l1 * Math.sin(RootAngle) + l2 * Math.sin(SumAngle);
+     * 回転行列をかける <a href="https://mathlandscape.com/rotation-matrix/">rotation matrix</a>
+     *
+     * @param theta 回転する角度[deg]
+     * @param x     x座標[cm]
+     * @param y     y座標[cm]
+     * @return 回転先の位置ベクトル <br>（x_dash,<br>&emsp;y_dash）
+     */
+    public static Map<Integer, Double> rotateMatrix(double theta, double x, double y) {
+        theta = Math.toRadians(theta); // [rad]
+        double x_dash = x * Math.cos(theta) - y * Math.sin(theta);
+        double y_dash = x * Math.sin(theta) + y * Math.cos(theta);
+        Map<Integer, Double> vector = new HashMap<>();
+        vector.put(0, x_dash);
+        vector.put(1, y_dash);
+        return vector;
     }
 
     /**
-     * @param RootAngle readSensorで取得した実際の角度[deg]
-     * @param JointAngle readSensorで取得した実際の角度[deg]
+     * @param theta_r readSensorで取得した実際の角度[deg]
+     * @param theta_j readSensorで取得した実際の角度[deg]
      * @return Depth座標[cm]
-     * */
-    public static double calculateDepth(double RootAngle, double JointAngle) {
-        RootAngle = Math.toRadians(RootAngle);
-        JointAngle = Math.toRadians(JointAngle);
-        double SumAngle = RootAngle + JointAngle;
-        double l1 = Const.Arm.RootArmLength;
-        double l2 = Const.Arm.HeadArmLength;
+     */
+    public static double calculateDepth(double theta_r, double theta_j) {
+        Map<Integer, Double> positionVec = calculatePositionVec(theta_r, theta_j);
+        return positionVec.get(0);
+    }
 
-        return l1 * Math.cos(RootAngle) + l2 * Math.cos(SumAngle);
+    /**
+     * @param theta_r RootAngle - readSensorで取得した実際の角度[deg]
+     * @param theta_j JointAngle - readSensorで取得した実際の角度[deg]
+     * @return Height座標[cm]
+     */
+    public static double calculateHeight(double theta_r, double theta_j) {
+        Map<Integer, Double> positionVec = calculatePositionVec(theta_r, theta_j);
+        return positionVec.get(1);
+    }
+
+    /**
+     * @param theta_r readSensorで取得した実際の角度[deg]
+     * @param theta_j readSensorで取得した実際の角度[deg]
+     * @return アームの先端の座標の位置ベクトル <br>
+     * 0 - X座標 - 奥行き（Depth）[cm]<br>
+     * 1 - Y座標 - 高さ（Height）[cm]<br>
+     */
+    public static Map<Integer, Double> calculatePositionVec(double theta_r, double theta_j) {
+        double theta_c = Const.Arm.HandFoldAngle; // [deg]
+
+        double l_r = Const.Arm.RootArmLength;
+        double l_j = Const.Arm.HeadArmLength;
+        double l_h = Const.Arm.HandLength;
+
+        Map<Integer, Double> rootVec = rotateMatrix(theta_r, l_r, 0.0);
+        Map<Integer, Double> headVec = rotateMatrix(theta_r + theta_j, l_j, 0.0);
+        Map<Integer, Double> handVec = rotateMatrix(theta_c + theta_r + theta_j, l_h, 0.0);
+
+        Map<Integer, Double> positionVec = new HashMap<>();
+        positionVec.put(0, rootVec.get(0) + headVec.get(0) + handVec.get(0));
+        positionVec.put(1, rootVec.get(1) + headVec.get(1) + handVec.get(1));
+
+        return positionVec;
     }
 
     /** コントローラーの不感帯の大きさ（絶対値）[0.0) */
@@ -40,71 +75,69 @@ public class Tools {
     /**
      * 不感帯処理関数
      * 絶対値がdeadZoneThreshold未満のものを淘汰
+     *
      * @param input コントローラーの値を入力
      * @return 不感帯処理を施したinput
-     * */
+     */
     public static double deadZoneProcess(double input) {
-        if(Math.abs(input) < deadZoneThreshold) return 0.0;
+        if (Math.abs(input) < deadZoneThreshold) return 0.0;
         else return input;
     }
 
     /**
-     * @param Depth ターゲットのX座標[cm]
-     * @param Height ターゲットのY座標[cm]
-     * X,YからRootAngle, JointAngle を計算
+     * @param x ターゲットの奥行き（Depth）[cm]
+     * @param y ターゲットの高さ（Height）[cm]<br>
+     *          Target[Depth/Height]からTarget[Root/Joint]Angleを計算
+     *          関数内はすべて[rad] 出力はすべて[deg]で統一
      * @return アームのターゲットの角度[deg]
      */
-    public static Map<String, Double> calculateAngles(double Depth, double Height) {
-        double l1 = Const.Arm.RootArmLength;
-        double l2 = Const.Arm.HeadArmLength;
-
-        double DepthPM = Math.signum(Depth);
-        double HeightPM = Math.signum(Height);
-
-        Depth = Math.abs(Depth);
-        Height = Math.abs(Height);
-
-        double JointAngle = Math.acos(Math.min((Math.pow(Height, 2) + Math.pow(Depth, 2)
-                - Math.pow(l1, 2) - Math.pow(l2, 2)) / (2 * l1 * l2), 1.0));
-
-        double tX = l1 + l2 * Math.cos(JointAngle);
-        double tY = l2 * Math.sin(JointAngle);
-        double r = Math.sqrt(Math.pow(tX, 2) + Math.pow(tY, 2));
-
-        double alpha = Math.acos(tX / r);
-
-        Map<String, Double> angles = new HashMap<String, Double>();
-
-        // theta2 < 0の時
-        double RootAngleM = Math.acos(Math.max(Math.min(Depth / r, 1.0), -1.0)) + alpha;
-        // theta2 > 0の時
-        double RootAngleP = Math.acos(Math.max(Math.min(Depth / r, 1.0), -1.0)) - alpha;
-
-        if (HeightPM >= 0 && DepthPM >= 0) {
-            angles.put("RootAngle", Math.toDegrees(RootAngleM));
-            angles.put("JointAngle", Math.toDegrees(-1 * JointAngle));
-        } else if (HeightPM < 0 && DepthPM >= 0) {
-            angles.put("RootAngle", Math.toDegrees(-1 * RootAngleP));
-            angles.put("JointAngle", Math.toDegrees(-1 * JointAngle));
-        } else if (HeightPM < 0 && DepthPM < 0) {
-            System.out.println(Math.toDegrees(RootAngleM));
-            angles.put("RootAngle", Math.toDegrees(RootAngleM) - 180);
-            angles.put("JointAngle", -1 * Math.toDegrees(JointAngle));
+    public static Map<String, Double> calculateAngles(double x, double y) {
+        double theta_j_pm;
+        if (x >= 0 & y >= 0) {
+            // 第1象限
+            theta_j_pm = -1;
+        } else if (x < 0 & y >= 0) {
+            // 第2象限
+            theta_j_pm = 1;
+        } else if (x < 0 & y < 0) {
+            // 第3象限
+            theta_j_pm = -1;
         } else {
-            angles.put("RootAngle", 180 - Math.toDegrees(RootAngleP));
-            angles.put("JointAngle", -1 * Math.toDegrees(JointAngle));
+            // 第4象限
+            theta_j_pm = -1;
         }
 
-        return angles;
+        double l_r = Const.Arm.RootArmLength;
+        double l_v = Const.Arm.VirtualHeadArmLength;
+
+        double theta_h = Math.toRadians(Const.Arm.VirtualArmFoldAngle); // [rad]
+        double theta_j = theta_j_pm * Math.acos((Math.pow(x, 2) + Math.pow(y, 2)
+                - Math.pow(l_r, 2) - Math.pow(l_v, 2)) / (2 * l_r * l_v)) // [rad]
+                - theta_h; // [rad]
+        double theta_arg_target = Math.atan2(
+                y,
+                x
+        ); // [rad]
+        double theta_arg_zero = Math.atan2(
+                l_v * Math.sin(theta_j + theta_h),
+                l_r + l_v * Math.cos(theta_j + theta_h)
+        ); // [rad]
+        double theta_r = theta_arg_target - theta_arg_zero; // [rad]
+
+        Map<String, Double> angles = new HashMap<>();
+        angles.put("RootAngle", Math.toDegrees(theta_r));
+        angles.put("JointAngle", Math.toDegrees(theta_j));
+
+        return angles; // [deg]
     }
 
     /**
-     * @param RootAngle readSensorで取得した実際の角度[deg]
+     * @param RootAngle  readSensorで取得した実際の角度[deg]
      * @param JointAngle readSensorで取得した実際の角度[deg]
-     * underMotorのフィードフォワードを計算
-     * それぞれのアームの重心をConstから取得
+     *                   underMotorのフィードフォワードを計算
+     *                   それぞれのアームの重心をConstから取得
      * @return モーメント[N*cm]
-     * */
+     */
     public static double calculateRootMotorFeedforward(double RootAngle, double JointAngle) {
         RootAngle = Math.toRadians(RootAngle);
         JointAngle = Math.toRadians(JointAngle);
@@ -126,12 +159,12 @@ public class Tools {
     }
 
     /**
-     * @param RootAngle readSensorで取得した実際の角度[deg]
+     * @param RootAngle  readSensorで取得した実際の角度[deg]
      * @param JointAngle readSensorで取得した実際の角度[deg]
-     * topMotorのフィードフォワードを計算
-     * 重心などをConstから取得
+     *                   topMotorのフィードフォワードを計算
+     *                   重心などをConstから取得
      * @return モーメント[N*cm]
-     * */
+     */
     public static double calculateJointMotorFeedforward(double RootAngle, double JointAngle) {
         RootAngle = Math.toRadians(RootAngle);
         JointAngle = Math.toRadians(JointAngle);
@@ -145,16 +178,18 @@ public class Tools {
     /**
      * NEOモーターのトルクとRPMの関係を利用 <a href="https://www.revrobotics.com/content/docs/REV-21-1650-DS.pdf">NEOのデータシート</a>
      * [注意] NEOモーターに合わせて出力する
+     *
      * @param torque トルク[N*cm] = モーメント / Const.Arms.[Under/Top]MotorGearRatio（ギア比に合わせて入力）
      * @return motor.setへの入力[-1.0, 1.0] (CANSparkMax)
-     * */
-    public static double changeTorqueToMotorInput (double torque) {
+     */
+    public static double changeTorqueToMotorInput(double torque) {
         return torque / Const.Arm.MotorMaxTorque;
         // TODO 2次関数的にトルクを求める必要があるらしい？
     }
 
     public static void main(String[] args) {
         State.StateReset();
+
         double targetDepth = -10;//State.Arm.TargetDepth.TopCorn;
         double targetHeight = -60;//Const.Calculation.Limelight.TopGoalHeight - Const.Arm.RootHeightFromGr;
         System.out.println(targetDepth);
@@ -162,10 +197,51 @@ public class Tools {
         System.out.println(isNewTargetPositionInLimit(targetHeight, targetDepth));
         Map<String, Double> map = calculateAngles(targetDepth, targetHeight);
         System.out.println(map);
-        double joint = map.get("JointAngle");
-        double root = map.get("RootAngle");
-        System.out.println(calculateDepth(root, joint));
-        System.out.println(calculateHeight(root, joint));
+//        double joint = map.get("JointAngle");
+//        double root = map.get("RootAngle");
+//        double joint = -90;
+//        double root = 0;
+//        System.out.println(calculateDepth(root, joint));
+//        System.out.println(calculateHeight(root, joint));
+
+        double l_r = Const.Arm.RootArmLength;
+        double l_j = Const.Arm.HeadArmLength;
+        double l_h = Const.Arm.HandLength;
+
+        double theta_r = 0;
+        double theta_j = 0;
+        double theta_c = Const.Arm.HandFoldAngle;
+
+        Map<Integer, Double> vec = rotateMatrix(theta_c + theta_r + theta_j, l_h, 0.0);
+        System.out.println(vec);
+
+        int correct = 0;
+        int error = 0;
+        int all = 0;
+        for (double root = -180; root <= 180; root += 1) {
+            for (double joint = -180; joint <= 180; joint += 1) {
+                all++;
+                Map<String, Double> angles = calculateAngles(calculateDepth(root, joint), calculateHeight(root, joint));
+                double p_root = angles.get("RootAngle");
+                double p_joint = angles.get("JointAngle");
+                if (isLossInTolerance(calculateDepth(root, joint), calculateDepth(p_root, p_joint), 0.1) && isLossInTolerance(calculateHeight(root, joint), calculateHeight(p_root, p_joint), 0.1)) {
+                    correct++;
+                } else {
+                    System.out.println(root);
+                    System.out.println(joint);
+                    error++;
+                }
+            }
+        }
+
+        System.out.printf("Correct : %d / %d\n", correct, all);
+        System.out.printf("Incorrect : %d / %d\n", error, all);
+        System.out.printf("CorrectRate : %d\n", correct / all);
+        System.out.printf("IncorrectRate : %d\n", error / all);
+    }
+
+    private static boolean isLossInTolerance(double original, double predicted, double tolerance) {
+        return Math.abs(original - predicted) < tolerance;
     }
 
     private static boolean isNewTargetPositionInLimit(double Height, double Depth) {
