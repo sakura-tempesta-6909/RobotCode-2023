45302b2981e686c3162e3bc30ec348608250521a
Team-PC-6909 <team_pc@sakura-tempesta.org>
2023-04-05 16:49:48 +0900

    数値調整

--- src/main/java/frc/robot/consts/ArmConst.java
+++ src/main/java/frc/robot/consts/ArmConst.java
@@ -246,11 +246,11 @@ public class ArmConst {
     /**
      * アームの理想的な高さ
      */
-    public static final double InitialHeight = -100;
+    public static final double InitialHeight = -90;
     /**
      5 アームの理想的な奥行き
      */
-    public static final double InitialDepth = -5;
+    public static final double InitialDepth = 5;
 
     /**
      * アームの根本の高さ[cm]（地面から） -> 座標の原点の高さ
--- src/main/java/frc/robot/consts/HandConst.java
+++ src/main/java/frc/robot/consts/HandConst.java
@@ -26,7 +26,7 @@ public class HandConst {
      */
     public static final double HandGearRatio = 12 * 40 / 24;
 
-    public static final double PIDAngleTolerance = 5;
+    public static final double PIDAngleTolerance = 10;
 
     public static void HandConstInit() {
 
